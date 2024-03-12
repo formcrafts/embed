54c81c2ab5282c1851a417c67b272c155f32e1c3
nCrafts <nishantagrawal234@gmail.com>
2024-03-12 17:30:59 +0100

    test

--- src/embed.ts
+++ src/embed.ts
@@ -2,6 +2,7 @@ import Drawer from "./drawer.ts";
 import css from "./embed.css";
 
 function getCookie(name) {
+  if (typeof document === "undefined") return;
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(";").shift();
